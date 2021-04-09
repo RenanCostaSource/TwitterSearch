94cd2d091c012d7df86fcf9056e952bc86733c5c
DESKTOP-705AG99\Zenic <renan.paiva@lavid.ufpb.br>
2021-04-09 13:51:59 -0300

    remove night mode

--- app/src/main/java/com/fuze/ttapp/ui/MainActivity.kt
+++ app/src/main/java/com/fuze/ttapp/ui/MainActivity.kt
@@ -2,12 +2,13 @@ package com.fuze.ttapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.android.AndroidInjection
 
 abstract class MainActivity<out VM : BaseViewModel> : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
-
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             if (hasInjector())
                 AndroidInjection.inject(this)
         }
